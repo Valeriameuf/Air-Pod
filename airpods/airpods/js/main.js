119f334e0f0c4fa1e128d8521bf1ba705ef3fb74
Valeriameuf <130457046+Valeriameuf@users.noreply.github.com>
2023-10-25 12:43:21 +0300

    Add files via upload

---
+++ airpods/airpods/js/main.js
@@ -0,0 +1,24 @@
+const chooseColor = document.querySelectorAll(".section-choose__btn");
+const contentItem = document.querySelectorAll(".content-item");
+
+chooseColor.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+
+function open(evt) {
+  const target = evt.currentTarget;
+  const button = target.dataset.button;
+  const contentActive = document.querySelectorAll(`.${button}`);
+
+  chooseColor.forEach(function (item) {
+    item.classList.remove("choose-color__btn--active");
+  });
+  target.classList.add("choose-color__btn--active");
+
+  contentItem.forEach(function (item) {
+    item.classList.remove("content-item--active");
+  });
+  contentActive.forEach(function (item) {
+    item.classList.add("content-item--active");
+  });
+}
